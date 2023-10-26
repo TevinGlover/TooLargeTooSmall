6471a683f46918add2f831f48b514af1ed01d185
Tevin Glover <tevwork.tg@gmail.com>
2023-10-26 18:51:34 -0400

    working it out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,13 @@
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    private int n =0;
+    private int i =0;
+
+    while (i != 3){
+        System.out.println(i);
+        i++;
+    }
 
     public static void main(String[] args){
         
