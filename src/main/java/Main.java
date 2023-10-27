0c93b9ed8ff353adf3f45f52aaa2a452736f5482
Tevin Glover <tevwork.tg@gmail.com>
2023-10-27 09:10:52 -0400

    now it run infintly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,26 @@
 import java.util.Scanner;
 public class Main {
 
-  // Scanner to add user input
-  Scanner scanner = new Scanner(System.in);
-  System.out.println("enter your guess 1 to 15")
-  static int guess = scanner.nextInt();
-  // the max number you can guess
-  int max = 15;
-  //random number for game
-  int randomNumber = (int) (Math.random() * max);
 
-  public static void main(String[] args) {
+
+  public static void main(String[] args) { // must write code in side here below
+    // Scanner to add user input
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter your guess 1 to 15");
+    int guess = scanner.nextInt();
+    // the max number you can guess
+    int max = 15;
+    //random number for game
+    int randomNumber = (int) (Math.random() * max);
+
     while (true) {
 //if else statement
       if (guess < randomNumber) {
         System.out.println("too small");
+        continue;
       } else if (guess > randomNumber) {
         System.out.println("too large");
+        continue;
       } else {
         System.out.println("correct guess" );
         break;
