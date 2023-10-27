40f1312173bfcff853165a588b8df72cd5a99190
Tevin Glover <tevwork.tg@gmail.com>
2023-10-26 20:47:05 -0400

    a little confused

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,31 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-/* whileloop
+//whileloop
 
-if else statement
+  Scanner scanner = new Scanner(System.in);
+  System.out.print("Enter you number")
+  int guess = scanner.nextInt();
 
-random number for user input
+  int max = 15;
+  int randomNumber = (int) (Math.random() * max);
+//if else statement
+  public static void main(String[] args) {
+    int guess = randomNumber;
+    if (guess < 6) {
+      System.out.println("too small");
+    } else if (guess > 8) {
+      System.out.println("too large");
+    }  else {
+      System.out.println("correct guess");
+    }
+  }
+}
 
- */
+//random number for user input
+
     public static void main(String[] args){
         
     }
