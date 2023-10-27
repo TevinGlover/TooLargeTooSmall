6bffaa9926c0fc91f4919e02e76123e91ca2e9f4
Tevin Glover <tevwork.tg@gmail.com>
2023-10-27 19:52:59 -0400

    counterNext

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,25 +10,31 @@ public class Main {
   public static void main(String[] args) { // must write code in side here below
     // Scanner to add user input
     Scanner scanner = new Scanner(System.in);
-    System.out.println("enter your guess 1 to 15");
+
+    System.out.println("enter your guess 1 to 15:");
     int guess = scanner.nextInt();
     // the max number you can guess
     int max = 15;
     //random number for game
     int randomNumber = (int) (Math.random() * max);
-
-    while (true) {
+    //the number of try a person try's to guess.
+    //int trycounter = 0; //add a to player try counter
+    while(true) {
 //if else statement
+
       if (guess < randomNumber) {
         System.out.println("too small");
-        continue;
+        guess = scanner.nextInt();// this pause the loop and allows u to enter another guess. restarting the loop.
       } else if (guess > randomNumber) {
         System.out.println("too large");
-        continue;
+        guess = scanner.nextInt();// this pause the loop and allows u to enter another guess. restarting the loop.
       } else {
         System.out.println("correct guess" );
-        break;
+        break;// this stops the loop.
+
+
       }
     }
   }
+
 }
