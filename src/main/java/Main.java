11d08f83fb98e449f3c50a60d16c2073bd98dfeb
Tevin Glover <tevwork.tg@gmail.com>
2023-10-27 08:57:11 -0400

    fixed some more error in code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,30 +4,27 @@
 
 import java.util.Scanner;
 public class Main {
+
   // Scanner to add user input
   Scanner scanner = new Scanner(System.in);
-  System.out.println("enter your guess 1 to 15" )
-  int guess = scanner.nextInt();
+  System.out.println("enter your guess 1 to 15")
+  static int guess = scanner.nextInt();
   // the max number you can guess
   int max = 15;
-
   //random number for game
-  int randomNumber = (int) (Math.random() * max +1);
-  //while loop
-  while(true){
-    System.out.prinIn("enter your guess 1 to 15")
+  int randomNumber = (int) (Math.random() * max);
+
+  public static void main(String[] args) {
+    while (true) {
 //if else statement
-    public static void main (String[]args){
-    if ( guess < randomNumber){
-      System.out.println("too small");
-    } else if (guess > randomNumber) {
-      System.out.println("too large");
-    }  else {
-      System.out.println("correct guess" + );
-      break;
+      if (guess < randomNumber) {
+        System.out.println("too small");
+      } else if (guess > randomNumber) {
+        System.out.println("too large");
+      } else {
+        System.out.println("correct guess" );
+        break;
+      }
     }
   }
-  }
 }
-
-
